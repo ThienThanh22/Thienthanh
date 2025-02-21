ec83c24aad5c3a17a2ecfa97f921619af2ac1038
ThienThanh22 <dnthienthanh2004@gmail.com>
2025-02-21 10:17:45 +0700

    Create scrip.js

---
+++ scrip.js
@@ -0,0 +1,11 @@
+function login()
+{ 
+  let u = username.value
+  let p = pw.value
+  if (u=="thanh" && p=="teamo")
+  {
+    window.open("Home.html)
+    document getElementById(compiler).style.display="block"
+  }
+
+                             
